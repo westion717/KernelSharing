a288d0844cbeafa684aae92fc1ab0ca4e71e86c2
Westion <westion717@gmail.com>
2014-10-26 19:07:08 +0800

    finish day03 src

---
+++ day03/src/include/system.h
@@ -0,0 +1,30 @@
+/* bkerndev - Bran's Kernel Development Tutorial
+*  By:   Brandon F. (friesenb@gmail.com)
+*  Desc: Global function declarations and type definitions
+*
+*  Notes: No warranty expressed or implied. Use at own risk. 
+*  modified: for tutorial of kernel.wuzx.me
+*  By:Westion westion717@gmail.com
+*/
+
+#include <type.h>
+
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+
+/*system.c*/
+//内存拷贝，从原地址拷贝count个字节到目标地址
+PUBLIC void *memcpy(void *dest, const void *src, size_t count);
+//内存赋值,从指定开始每个内存单元都赋值为val
+PUBLIC void *memset(void *dest, char val, size_t count);
+//内存赋值（字节）从指定开始每个内存单元都赋值为val
+PUBLIC u16 *memsetw(u16 *dest, u16 val, size_t count);
+//计算字符串长度
+PUBLIC size_t strlen(const char *str);
+//IO操作，端口读值
+PUBLIC u8 inportb (u16 _port);
+//IO操作，端口赋值
+PUBLIC void outportb (u16 _port, u8 _data);
+
+#endif
---
+++ day03/src/source/main.c
@@ -0,0 +1,14 @@
+/* bkerndev - Bran's Kernel Development Tutorial
+*  By:   Brandon F. (friesenb@gmail.com)
+*  Desc: main.o: C code Entry
+*
+*  Notes: No warranty expressed or implied. Use at own risk. 
+*
+*  modified: for tutorial of kernel.wuzx.me
+*  By:Westion westion717@gmail.com
+*/
+
+void main()
+{
+    for (;;);
+}
